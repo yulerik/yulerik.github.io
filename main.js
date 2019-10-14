7fa018343399c5d16205e07397ce9290e58fa9fc
Yulerik Sorensen <yulerik.sorensen@gmail.com>
2019-10-14 14:14:37 -0700

    Javascript
    adding color to clicks

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("mainHeader").onclick = function() {
       this.style.color = 'orange'
     }
-  })
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementsByTagName("h1").onclick = function() {
+      this.style.color = 'orange'
+    }
+})
